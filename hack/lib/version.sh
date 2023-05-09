86623c5ee49c3ec9f6f443f4f61ccd6fb183258c
Kevin Chevreuil - Kaisen <kaisen@kaisenlinux.org>
2023-05-09 10:51:59 +0200

    Import the 1.25.9 version

--- hack/lib/version.sh
+++ hack/lib/version.sh
@@ -43,12 +43,12 @@ kube::version::get_version_vars() {
   # Disabled as we're not expanding these at runtime, but rather expecting
   # that another tool may have expanded these and rewritten the source (!)
   if [[ '%' == "%" ]]; then
-    KUBE_GIT_COMMIT='0ce7342c984110dfc93657d64df5dc3b2c0d1fe9'
+    KUBE_GIT_COMMIT='a1a87a0a2bcd605820920c6b0e618a8ab7d117d4'
     KUBE_GIT_TREE_STATE="archive"
-    # When a 'git archive' is exported, the 'tag: v1.25.8' below will look
+    # When a 'git archive' is exported, the 'tag: v1.25.9' below will look
     # something like 'HEAD -> release-1.8, tag: v1.8.3' where then 'tag: '
     # can be extracted from it.
-    if [[ 'tag: v1.25.8' =~ tag:\ (v[^ ,]+) ]]; then
+    if [[ 'tag: v1.25.9' =~ tag:\ (v[^ ,]+) ]]; then
      KUBE_GIT_VERSION="${BASH_REMATCH[1]}"
     fi
   fi
